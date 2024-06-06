cfc161114930de4178c8d6f264088f584124114e
OlegKryvohub <2103kokakola2004@gmail.com>
2024-06-07 01:13:45 +0300

    add data in profile

--- src/pages/ProfilePages.jsx
+++ src/pages/ProfilePages.jsx
@@ -4,6 +4,9 @@ import Product from '../components/Product';
 import { Context } from '..';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import Loader from '../components/Loader';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/storage';
+import { FaPenAlt } from "react-icons/fa";
 
 export default function ProfilePages() {
   const { auth, firestore } = useContext(Context);
@@ -11,12 +14,17 @@ export default function ProfilePages() {
   const [products, setProducts] = useState([]);
   const [cartItems, setCartItems] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [displayName, setDisplayName] = useState('');
+  const [photoURL, setPhotoURL] = useState('');
+  const [file, setFile] = useState(null);
+  const [previewURL, setPreviewURL] = useState('');
+  const [updating, setUpdating] = useState(false);
+  const [showDisplayName, setShowDisplayName] = useState(true);
 
   useEffect(() => {
     if (user) {
       const fetchUserData = async () => {
         try {
-          // Fetch user products
           const productsRef = firestore.collection("users").doc(user.uid).collection("products");
           const productsSnapshot = await productsRef.get();
           const productsList = productsSnapshot.docs.map(doc => ({
@@ -24,13 +32,11 @@ export default function ProfilePages() {
             ...doc.data(),
           }));
 
-          // Fetch user cart items
           const cartRef = firestore.collection("carts").doc(user.uid);
           const cartSnapshot = await cartRef.get();
           const cartData = cartSnapshot.data();
           const cartItemsList = cartData ? cartData.products : [];
 
-          // Fetch product details for each cart item
           const cartProducts = await Promise.all(cartItemsList.map(async (productId) => {
             const productDoc = await firestore.collection("products").doc(productId).get();
             return { id: productDoc.id, ...productDoc.data() };
@@ -38,6 +44,8 @@ export default function ProfilePages() {
 
           setProducts(productsList);
           setCartItems(cartProducts);
+          setDisplayName(user.displayName);
+          setPhotoURL(user.photoURL);
           setLoading(false);
         } catch (error) {
           console.error("Error fetching user data: ", error);
@@ -49,15 +57,101 @@ export default function ProfilePages() {
     }
   }, [user, firestore]);
 
-  if (loading) {
-    return <Loader />
-  }
+  const handleUpdateProfile = async (e) => {
+    e.preventDefault();
+    setUpdating(true);
+    try {
+      const currentUser = firebase.auth().currentUser;
+      if (currentUser) {
+        let photoURLToUpdate = photoURL;
+
+        if (file) {
+          const storageRef = firebase.storage().ref();
+          const fileRef = storageRef.child(`profilePictures/${currentUser.uid}/${file.name}`);
+          await fileRef.put(file);
+          photoURLToUpdate = await fileRef.getDownloadURL();
+        }
+
+        await currentUser.updateProfile({
+          displayName,
+          photoURL: photoURLToUpdate,
+        });
+
+        await firestore.collection('users').doc(currentUser.uid).set({
+          displayName,
+          photoURL: photoURLToUpdate,
+        }, { merge: true });
+
+        setLoading(false);
+        setPreviewURL('');
+        setFile(null);
+        setUpdating(false);
+        setShowDisplayName(true);
+
+
+      }
+    } catch (error) {
+      console.error("Error updating profile: ", error);
+      setUpdating(false);
+      setShowDisplayName(true);
+
+    }
+  };
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+      setPreviewURL(URL.createObjectURL(selectedFile));
+    }
+  };
+
+  const handleCancel = () => {
+    setDisplayName(user.displayName);
+    setPhotoURL(user.photoURL);
+    setPreviewURL('');
+    setFile(null);
+    setShowDisplayName(true);
+  };
 
   return (
     <div className='profilePages'>
-      <div className='profilePages__ti'>
-        <h1 className='profilePages__title'>{user.displayName}</h1>
-        <img src={user.photoURL} alt="avatar" />
+      <div className='profilePages__title'>
+        <form className='profilePages__form' onSubmit={handleUpdateProfile}>
+          <div className='profilePages__inputs'>
+
+            <div className='profilePages__formContainer'>
+              <label htmlFor="displayName"><FaPenAlt className='profilePages__icon' onClick={() => setShowDisplayName(!showDisplayName)} /></label>
+              {showDisplayName ?
+                <h1>{displayName}</h1>
+                : <input
+                  type="text"
+                  id="displayName"
+                  value={displayName}
+                  onChange={(e) => setDisplayName(e.target.value)}
+                  className='input'
+                />
+              }
+
+
+            </div>
+            <div className='profilePages__formContainer'>
+              <label htmlFor="photoFile"><FaPenAlt className='profilePages__icon' /></label>
+              <img src={previewURL || photoURL} alt="avatar" className='profilePages__avatar' />
+              <input
+                type="file"
+                id="photoFile"
+                onChange={handleFileChange}
+              />
+            </div>
+          </div>
+          {updating && <Loader />}
+          <div className="profilePages__buttons">
+            <button type="submit" className='button' disabled={updating}>Зберегти зміни</button>
+            <button type="button" className='button' onClick={handleCancel}>Скасувати зміни</button>
+          </div>
+
+        </form>
         <div className='productsList' id="productsList">
           <div className="productsList__container">
             <h1 className="productsList__title">Корзина</h1>
